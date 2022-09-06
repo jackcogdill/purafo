f2bf18cc6cb1d1a66079f49de781333edfd42fcb
Jack Cogdill <hi@jackcogdill.com>
2022-09-05 21:34:20 -0700

    Use webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,11 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.ts',
+  mode: 'development',
   devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
   module: {
     rules: [
       {
