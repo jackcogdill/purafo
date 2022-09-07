11bdd68e358ff636583ec610963891d3fd9ea79b
Jack Cogdill <hi@jackcogdill.com>
2022-09-06 23:56:48 -0700

    Create game class

---
+++ src/game.ts
@@ -0,0 +1,9 @@
+export class Game {
+  constructor(private readonly canvas: HTMLCanvasElement) {
+    console.log('Init game');
+  }
+
+  run() {
+    console.log('Running game');
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,16 @@
-const msg: string = 'Hello, World.';
-alert(msg);
+import { Game } from './game';
+
+(() => {
+  const canvas = document.querySelector('canvas');
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+
+  const game = new Game(canvas);
+  game.run();
+
+  window.addEventListener('resize', () => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    // game.doSomething()
+  });
+})();
